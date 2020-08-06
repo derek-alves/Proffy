7d8eeb7a98f9bfe055903d59a82d1b45745ae97b
derek-alves <derek.alves@fatec.sp.gov.br>
2020-08-06 18:18:47 -0300

    Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app  = express();
 app.use(express.json());
 
+app.use(routes)
 
-app.get('/users',(req,res) => {
-    return res.json('hello word');
-})
 app.listen(3333);
